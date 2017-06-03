dfc78290ccf1d5ab63b40ecf803f4f188302dc6c
chiguozi <qdlzr456@163.com>
2017-06-03 09:43:24 +0800

    换装package

--- Assets/NGUITest/Script/Main.cs
+++ Assets/NGUITest/Script/Main.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main : MonoBehaviour {
+namespace NGUITest
+{
+    public class Main : MonoBehaviour
+    {
 
-    Camera _camera;
-    Transform _cube;
-	// Use this for initialization
-	void Start () {
-        _camera = FindObjectOfType<Camera>();
-        _cube = GameObject.Find("Cube").transform;
-        _camera.GetSidesTest(Mathf.Lerp(_camera.nearClipPlane, _camera.farClipPlane, 0.5f), _cube);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.inputString == "@")
+        Camera _camera;
+        Transform _cube;
+        // Use this for initialization
+        void Start()
+        {
+            _camera = FindObjectOfType<Camera>();
+            _cube = GameObject.Find("Cube").transform;
             _camera.GetSidesTest(Mathf.Lerp(_camera.nearClipPlane, _camera.farClipPlane, 0.5f), _cube);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.inputString == "@")
+                _camera.GetSidesTest(Mathf.Lerp(_camera.nearClipPlane, _camera.farClipPlane, 0.5f), _cube);
+        }
     }
 }
